66fef6d70c5630461be7568b37e6ea979cc5cc37
Muhammad Romli <genbugenba@gmail.com>
2026-09-26 00:09:24 +0700

    wip: done making caching structure, untested

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -3,46 +3,53 @@ package pokecache
 import (
 	"sync"
 	"time"
-
-	"honnef.co/go/tools/lintcmd/cache"
 )
 
-type cacheEntry struct{
+type cacheEntry struct {
 	createdAt time.Time
-	val []byte
+	val       []byte
 }
 
 type Cache struct {
-	mu sync.Mutex
+	mu       sync.Mutex
 	cacheMap map[string]cacheEntry
 }
 
-func NewCache(interval time.Duration)  *Cache {
-	newCacheVar :=  &Cache{
+func NewCache(interval time.Duration) *Cache {
+	newCacheP := &Cache{
 		cacheMap: make(map[string]cacheEntry),
 	}
-	newCacheVar.cacheMap.
-	go newCacheVar.reset(interval)
+	go newCacheP.readLoop(interval)
 	return newCacheP
 }
 
-func (c Cache) reset (interval time.Duration) {
-	ticker := time.NewTicker(interval*time.Second)
-	for range <-ticker.C {
+func (c *Cache) readLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
 		c.mu.Lock()
-		for k,v := range c.cacheMap {
+		for k, v := range c.cacheMap {
 			if time.Since(v.createdAt) > interval {
-
+				delete(c.cacheMap, k)
 			}
 		}
 		c.mu.Unlock()
 	}
 }
 
-func (c Cache) Add (key string, val []byte) {
+func (c *Cache) Add(k string, v []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
+	c.cacheMap[k] = cacheEntry{
+		createdAt: time.Now(),
+		val:       v,
+	}
 }
 
-func (c Cache) Get (key string, val {}bytr) {
+func (c *Cache) Get(k string) (v []byte, isExist bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
+	entry, ok := c.cacheMap[k]
+	return entry.val, ok
 }
